3e8cb8c5836b066f5c57e15cdafe86f2204c455d
Boaz <15123825+boazhuisman@users.noreply.github.com>
2021-10-14 11:12:53 +0200

    Update service-endpoint to new ogc api tiles endpoint

--- js/service-endpoints.js
+++ js/service-endpoints.js
@@ -3,6 +3,6 @@ var serviceEndpoints = [
   { 'name': 'omgevingswet-demo', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-demo/wmts/v1_0/locaties/EPSG:28992' },
   { 'name': 'omgevingswet-pre', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-pre/wmts/v1_0/locaties/EPSG:28992' },
   { 'name': 'omgevingswet-prod', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten/wmts/v1_0/locaties/EPSG:28992' },
-  { 'name': 'bgt-demo', 'url': 'https://service.pdok.nl/bgt/demo/wmts/v1_0/locaties/EPSG:28992' }
+  { 'name': 'bgt-demo', 'url': 'https://api.pdok.nl/lv/bgt/oat/v1_0-preprod/tiles/EPSG:28992' }
 ]
 export default serviceEndpoints
