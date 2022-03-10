bd4077107ec58a4d6884b153709cfeaa3c73c669
Roel Arents <roel.arents@kadaster.nl>
2022-03-10 17:23:39 +0100

    no _RUNTIME_SERVICE_ENDPOINTS in github pages

--- js/service-endpoints.js
+++ js/service-endpoints.js
@@ -7,7 +7,10 @@ const defaultServiceEndpoints = [
   { 'name': 'bgt-demo', 'url': 'https://api.pdok.nl/lv/bgt/oat/v1_0-preprod/tiles/EPSG:28992/{z}/{x}/{y}' }
 ]
 
+const serviceEndpointsPlaceHolder = "_RUNTIME_SERVICE_ENDPOINTS"
 const serviceEndpointsJson = process.env.SERVICE_ENDPOINTS
-const serviceEndpoints = serviceEndpointsJson ? JSON.parse(serviceEndpointsJson) : defaultServiceEndpoints
+const serviceEndpoints = (serviceEndpointsJson && serviceEndpointsJson !== serviceEndpointsPlaceHolder)
+  ? JSON.parse(serviceEndpointsJson)
+  : defaultServiceEndpoints
 
 export default serviceEndpoints
