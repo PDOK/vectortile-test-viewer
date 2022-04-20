089919302fd9c2dff40f2148c6317db40f939069
Roel Arents <roel.arents@kadaster.nl>
2022-04-20 13:58:31 +0200

    fix service endpoints runtime injection

--- docker-entrypoint.d/runtime-env.sh
+++ docker-entrypoint.d/runtime-env.sh
@@ -4,5 +4,10 @@ set -eu
 
 cd /usr/share/nginx/html
 
-grep -oP '(?<=src=")index\.[a-f0-9]{8}\.js(?=")' index.html > /tmp/js_path
-sed -i 's#"_RUNTIME_SERVICE_ENDPOINTS"#'"'$SERVICE_ENDPOINTS'#g" "$(cat /tmp/js_path)"
+js_path=$(grep -oP '(?<=src=")index\.[a-f0-9]{8}\.js(?=")' index.html)
+count=$(grep -c _RUNTIME_SERVICE_ENDPOINTS "$js_path")
+echo replacing $count occurences of _RUNTIME_SERVICE_ENDPOINTS in "$js_path"
+echo SERVICE_ENDPOINTS:
+echo $SERVICE_ENDPOINTS
+sed -i 's#"_RUNTIME_SERVICE_ENDPOINTS"#'"'$SERVICE_ENDPOINTS'#" "$js_path"
+sed -i 's#"_RUNTIME_SERVICE_ENDPOINTS"#'"'$SERVICE_ENDPOINTS'#" "$js_path.map"
--- js/service-endpoints.js
+++ js/service-endpoints.js
@@ -7,9 +7,9 @@ const defaultServiceEndpoints = [
   { 'name': 'bgt-demo', 'url': 'https://api.pdok.nl/lv/bgt/oat/v1_0-preprod/tiles/EPSG:28992/{z}/{x}/{y}' }
 ]
 
-const serviceEndpointsPlaceHolder = "_RUNTIME_SERVICE_ENDPOINTS"
-const serviceEndpointsJson = process.env.SERVICE_ENDPOINTS
-const serviceEndpoints = (serviceEndpointsJson && serviceEndpointsJson !== serviceEndpointsPlaceHolder)
+// FIXME Find a proper way to inject runtime env
+const serviceEndpointsJson = '' + (window['SERVICE_ENDPOINTS'] || '') + (process.env.SERVICE_ENDPOINTS || '')
+const serviceEndpoints = (serviceEndpointsJson && serviceEndpointsJson.length !== 26)
   ? JSON.parse(serviceEndpointsJson)
   : defaultServiceEndpoints
 
