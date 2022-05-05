30b48ac174aeda1be1f01eb196f9a4849c6c39c3
Roel Arents <roel.arents@kadaster.nl>
2022-05-05 13:49:09 +0200

    add ozon totaal and keten endpoints

--- js/service-endpoints.js
+++ js/service-endpoints.js
@@ -3,6 +3,14 @@ const defaultServiceEndpoints = [
   { 'name': 'omgevingswet-demo', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-demo/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
   { 'name': 'omgevingswet-pre', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-pre/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
   { 'name': 'omgevingswet-prod', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-ketenprf', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-ketenprf/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-ketenacc', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-ketenacc/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-acceptatie-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal-acc/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-demo-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal-demo/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-pre-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal-pre/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-prod-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-ketenprf-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal-ketenprf/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-ketenacc-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal-ketenacc/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
   { 'name': 'omgevingswet-preprod', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-preprod/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
   { 'name': 'bgt-demo', 'url': 'https://api.pdok.nl/lv/bgt/oat/v1_0-preprod/tiles/EPSG:28992/{z}/{x}/{y}' }
 ]
