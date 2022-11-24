bf46f6e5391027d58061c2a473068a1ab42cb8ad
Roel Arents <roel.arents@kadaster.nl>
2022-11-24 11:09:23 +0100

    change omgevingswet-preprod service url

--- js/service-endpoints.js
+++ js/service-endpoints.js
@@ -13,7 +13,7 @@ const defaultServiceEndpoints = [
   { 'name': 'omgevingswet-prod-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
   { 'name': 'omgevingswet-ketenprf-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal-ketenprf/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
   { 'name': 'omgevingswet-ketenacc-totaal', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-totaal-ketenacc/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
-  { 'name': 'omgevingswet-preprod', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten-preprod/wmts/v1_0/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
+  { 'name': 'omgevingswet-preprod', 'url': 'https://service.pdok.nl/omgevingswet/omgevingsdocumenten/wmts/v1_0-preprod/locaties/EPSG:28992/{z}/{x}/{y}.pbf' },
   { 'name': 'bgt-demo', 'url': 'https://api.pdok.nl/lv/bgt/ogc/v0_1/tiles/NetherlandsRDNewQuad/{z}/{x}/{y}.pbf' }
 ]
 
